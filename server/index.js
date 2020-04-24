398030419c71bf1814fc5c21f6f7f76251a32d9d
oliverjam <oliverjamesphillips@gmail.com>
2020-04-24 15:15:31 +0100

    Make sure we crash on unhandled promise rejections

--- server/index.js
+++ server/index.js
@@ -3,3 +3,8 @@ const server = require("./server");
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+
+process.on("unhandledRejection", (error) => {
+  console.error(error);
+  process.exit(1);
+});
